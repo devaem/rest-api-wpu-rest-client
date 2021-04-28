92808e852de115c51d924a7b678273e66762027c
devaega13@gmail.com <devaega13@gmail.com>
2021-04-28 11:19:31 +0700

    latihan rest-api-wpu-rest-client wpu

---
+++ application/views/home/index.php
@@ -0,0 +1,13 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-8">
+			
+	    	<h3>Hello, Nama Saya Deva Ega Marinda!</h3>
+	    	<h4>NIM Saya 19.01.53.0001</h4>
+	    	<h4>Mata Kuliah Web Service E1</h4>
+		</div>
+
+	</div>
+</div>
+
+ 
